76569cb9e8c8dea3dd2f974719a2cfc8b55919b1
Parwana2021 <parwanaanwari1@gmail.com>
2026-03-26 15:58:28 -0400

    Update Helloworld.java

--- src/day01/Helloworld.java
+++ src/day01/Helloworld.java
@@ -3,6 +3,9 @@ package day01;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello CYDEO");
+        System.out.println("Hello Progarammers");
+        
     }
 
 }
